d4ae28bea14ca3a5c79490b56f5dc2aa57c7098f
Garrett Honeycutt <github@garretthoneycutt.com>
2010-06-25 15:31:29 -0700

    initial publication

---
+++ manifests/init.pp
@@ -0,0 +1,102 @@
+# Class: mysql
+#
+# This module manages mysql clients and provides tools for dealing with mysql
+#
+# Requires:
+#   class puppet
+#   $lsbProvider is set in site manifest
+#
+class mysql {
+
+    include puppet
+
+    $snippets = "/opt/$lsbProvider/mysql_snippets"
+
+    file { "$snippets":
+        ensure => directory,
+        mode   => "700",
+    } # file
+
+    package { "mysql": }
+
+    # Definition: mysql::do
+    #
+    # connect to a mysql host and run specified sql code
+    #
+    # Parameters:   
+    #   $host     - hostname, uses localhost by default
+    #   $port     - port number, uses 3306 by default
+    #   $content  - specify content or a template instead of a .sql file
+    #   $source   - .sql file to load
+    #   $user     - mysql username, uses root by default
+    #   $password - mysql password uses blank by default
+    #   $database - name of mysql database
+    #
+    # Actions:
+    #   connects to a mysql host and load sql code
+    #
+    # Requires:
+    #   must specify at least $source or $content
+    #
+    # Sample Usage:
+    #    # setup database 
+    #    mysql::do {
+    #        "openfire_db_create":
+    #            source  => "puppet:///modules/openfire/openfire.sql";
+    #        "openfire_db_setup":
+    #            require => Mysql::Do["openfire_db_create"],
+    #            database => "$openfireDB",
+    #            source  => "puppet:///modules/openfire/openfire_mysql.sql";
+    #    } # mysql::do
+    #
+    define do ($host = localhost, $port = 3306, $content = '', $source = '', $user = "root", $password = '', $database = '') {
+        file { 
+            "${mysql::snippets}/$name.sql":
+                mode   => "600";
+        } # file
+        
+        exec { "mysql-do-$name":
+            creates => "${puppet::semaphores}/$name.mysql",
+        } 
+
+        case $content {
+            '': {
+                File["${mysql::snippets}/$name.sql"] {
+                    source => "$source",
+                } # File
+            } # '' empty
+            default: {
+                File["${mysql::snippets}/$name.sql"] {
+                    content => "$content",
+                } # File
+            } # default
+        } # case $content
+        
+        case $password {
+            '': {
+                Exec["mysql-do-$name"] {
+                    command +> "mysql -u$user -h$host -P3306 $database < ${mysql::snippets}/$name.sql > ${puppet::semaphores}/$name.mysql"
+                } # Exec
+            } # '' empty
+            default: {
+                Exec["mysql-do-$name"] {
+                    command +> "mysql -u$user -h$host -P3306 -p$password $database < ${mysql::snippets}/$name.sql > ${puppet::semaphores}/$name.mysql",
+                } # Exec
+            } # default
+        } # case $password
+
+        case $host {
+            default: {
+                Exec["mysql-do-$name"] {
+                    require +> [ File["${mysql::snippets}/$name.sql"], File["${puppet::semaphores}"] ],
+                } # Exec
+            } # default
+            localhost: {
+                Exec["mysql-do-$name"] {
+                    require +> [ File["${mysql::snippets}/$name.sql"], File["${puppet::semaphores}"] ],
+                    #before  +> Exec["MySQL flush privileges"],
+                } # Exec
+            } # localhost
+        } # case $host
+    } # define do
+} # class mysql
---
+++ pkg/ghoneycutt-mysql-1.0.0/Modulefile
@@ -0,0 +1,6 @@
+name    'ghoneycutt-mysql'
+version '1.0.0'
+
+## Add dependencies, if any:
+dependency 'ghoneycutt/mylvmbackup', '>= 1.0.0'
+dependency 'ghoneycutt/puppet', '>= 1.0.0'
